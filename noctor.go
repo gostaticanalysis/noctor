12fb87dd715b1a404abb832215c1941c09a75f54
tenntenn <uedatakuya@gmail.com>
2021-08-10 18:17:56 +0900

    Fix bugs

--- noctor.go
+++ noctor.go
@@ -33,27 +33,91 @@ func isUnnecessaryConstructor(pass *analysis.Pass, decl ast.Decl) bool {
 	}
 
 	sig, _ := pass.TypesInfo.TypeOf(fundecl.Name).(*types.Signature)
-	return sig != nil && sig.Results().Len() == 1 &&
-		isStructOrPtrStruct(sig.Results().At(0).Type()) &&
-		onlyReturn(fundecl.Body)
+	if sig == nil || sig.Results().Len() != 1 {
+		return false
+	}
+
+	ret := sig.Results().At(0).Type()
+	return !isShortFuncName(fundecl.Name, ret) && isStructOrPtrStruct(ret) && onlySimpleReturn(fundecl.Body)
+}
+
+func isShortFuncName(id *ast.Ident, typ types.Type) bool {
+	named, _ := typ.(*types.Named)
+	if named == nil {
+		return false
+	}
+	return len(id.Name) < len(named.Obj().Id())
 }
 
 func isStructOrPtrStruct(typ types.Type) bool {
 	switch typ := typ.Underlying().(type) {
 	case *types.Struct:
-		return true
+		return onlyExportedFields(typ)
 	case *types.Pointer:
 		// no recursive: pointer of pointer become false
-		_, isStruct := typ.Elem().Underlying().(*types.Struct)
-		return isStruct
+		return onlyExportedFields(typ.Elem())
 	}
 	return false
 }
 
-func onlyReturn(body *ast.BlockStmt) bool {
-	if len(body.List) != 1 {
+func onlyExportedFields(typ types.Type) bool {
+	st, _ := typ.Underlying().(*types.Struct)
+	if st == nil {
 		return false
 	}
-	_, isRet := body.List[0].(*ast.ReturnStmt)
-	return isRet
+
+	for i := 0; i < st.NumFields(); i++ {
+		if !st.Field(i).Exported() {
+			return false
+		}
+	}
+
+	return true
+}
+
+func onlySimpleReturn(body *ast.BlockStmt) bool {
+	if body == nil || len(body.List) != 1 {
+		return false
+	}
+
+	ret, _ := body.List[0].(*ast.ReturnStmt)
+	if ret == nil || len(ret.Results) != 1 {
+		return false
+	}
+
+	clit := compositLit(ret.Results[0])
+	if clit == nil {
+		return false
+	}
+
+	for _, expr := range clit.Elts {
+		if !identOrLit(expr) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func compositLit(expr ast.Expr) *ast.CompositeLit {
+	switch expr := expr.(type) {
+	case *ast.CompositeLit:
+		return expr
+	case *ast.UnaryExpr:
+		return compositLit(expr.X)
+	}
+	return nil
+}
+
+func identOrLit(expr ast.Expr) bool {
+	switch expr := expr.(type) {
+	case *ast.Ident:
+		return true
+	case *ast.BasicLit:
+		return true
+	case *ast.KeyValueExpr:
+		return identOrLit(expr.Value)
+	default:
+		return false
+	}
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -52,3 +52,63 @@ func NewLit2(n int, s string) *struct { // want "unnecessary constructor like fu
 		S: s,
 	}
 }
+
+func NewLit3(n int, s string) struct { // OK
+	n int
+	S string
+} {
+	return struct {
+		n int
+		S string
+	}{
+		n: n,
+		S: s,
+	}
+}
+
+func NewLit4(n int, s string) struct { // OK
+	T
+} {
+	return struct {
+		T
+	}{T{
+		N: n,
+		S: s,
+	}}
+}
+
+func NewLit5(n int, s string) struct { // OK
+	*T
+} {
+	return struct {
+		*T
+	}{&T{
+		N: n,
+		S: s,
+	}}
+}
+
+type t = T
+
+func NewLit6(n int, s string) struct { // OK
+	t
+} {
+	return struct {
+		t
+	}{t{
+		N: n,
+		S: s,
+	}}
+}
+
+type Pointer struct {
+	X int
+	Y int
+}
+
+func Pt(x, y int) Pointer { // OK - short name
+	return Pointer{
+		X: x,
+		Y: y,
+	}
+}
